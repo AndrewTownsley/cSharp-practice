fc0510d6b7c04d33f2a0ba3c3fa5f2a2e6b5c138
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-12 10:01:44 -0600

    more loops, random number and character generating.

--- Esercises12/Program.cs
+++ Esercises12/Program.cs
@@ -2,9 +2,79 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Speed Limit:  ");
+
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                /*                Console.WriteLine(random.Next(1,100));*/
+                buffer[i] = (char)('a' + random.Next(0, 26));
+                var password = new string(buffer);
+                Console.WriteLine(password);    
+            
+
+
+
+
+           /* while (true)
+            {
+                Console.Write("Enter your Name...");
+                var nameInput = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(nameInput))
+                {
+                    Console.WriteLine("@Echo" + " " + nameInput);
+                    continue;
+                }
+            }*/
+
+
+            // While Loop
+         /*   var i = 0;
+
+            while(i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+                i++;
+            }*/
+
+            // foreach Loop
+
+            /*var stockArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            foreach( var num in stockArray)
+            {
+                if(num % 2 == 0)
+                Console.WriteLine(num);
+            }*/
+
+
+            // foreach Loop
+          /*  var userName = "Othello Ferrari";
+
+            foreach(var character in userName)
+            {
+                Console.WriteLine(character);
+            }*/
+
+
+
+          /*  for (var i = 0; i < userName.Length; i++)
+            {
+                Console.WriteLine(userName[i]);
+            }*/
+
+
+
+        /*    Console.WriteLine("Enter Speed Limit:  ");
             var limit = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Speed of Vehicle: ");
             var speed = Convert.ToInt32(Console.ReadLine());
@@ -22,7 +92,7 @@
                 Console.WriteLine("2 Points :<");
             }
             else if (speed - limit > 12)
-                Console.WriteLine("Suspended :o");
+                Console.WriteLine("Suspended :o");*/
 
 
             /*       Console.WriteLine("Please Height then Width of Image...");
